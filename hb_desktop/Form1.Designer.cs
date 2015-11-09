5efa4d69f20f0de441a2ea984c5baed96b018863
benwalcutt <bewalcut@email.uark.edu>
2015-11-09 10:15:25 -0600

    Initial building
    - AsyncTask under construction

---
+++ hb_desktop/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace hb_desktop
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.InventoryButton = new System.Windows.Forms.Button();
+            this.ClientsButton = new System.Windows.Forms.Button();
+            this.EmployeeButton = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.InventoryPanel = new System.Windows.Forms.Panel();
+            this.ClientPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            this.InventoryPanel.SuspendLayout();
+            this.ClientPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(868, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "E&dit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 391);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(868, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // InventoryButton
+            // 
+            this.InventoryButton.Location = new System.Drawing.Point(12, 27);
+            this.InventoryButton.Name = "InventoryButton";
+            this.InventoryButton.Size = new System.Drawing.Size(75, 23);
+            this.InventoryButton.TabIndex = 2;
+            this.InventoryButton.Text = "Inventory";
+            this.InventoryButton.UseVisualStyleBackColor = true;
+            this.InventoryButton.Click += new System.EventHandler(this.InventoryButton_Click);
+            // 
+            // ClientsButton
+            // 
+            this.ClientsButton.Location = new System.Drawing.Point(93, 27);
+            this.ClientsButton.Name = "ClientsButton";
+            this.ClientsButton.Size = new System.Drawing.Size(75, 23);
+            this.ClientsButton.TabIndex = 3;
+            this.ClientsButton.Text = "Clients";
+            this.ClientsButton.UseVisualStyleBackColor = true;
+            this.ClientsButton.Click += new System.EventHandler(this.ClientsButton_Click);
+            // 
+            // EmployeeButton
+            // 
+            this.EmployeeButton.Location = new System.Drawing.Point(174, 27);
+            this.EmployeeButton.Name = "EmployeeButton";
+            this.EmployeeButton.Size = new System.Drawing.Size(75, 23);
+            this.EmployeeButton.TabIndex = 4;
+            this.EmployeeButton.Text = "Employees";
+            this.EmployeeButton.UseVisualStyleBackColor = true;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.ClientPanel);
+            this.MainPanel.Controls.Add(this.InventoryPanel);
+            this.MainPanel.Location = new System.Drawing.Point(12, 56);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(844, 332);
+            this.MainPanel.TabIndex = 5;
+            // 
+            // InventoryPanel
+            // 
+            this.InventoryPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InventoryPanel.Controls.Add(this.dataGridView1);
+            this.InventoryPanel.Controls.Add(this.label1);
+            this.InventoryPanel.Location = new System.Drawing.Point(16, 12);
+            this.InventoryPanel.Name = "InventoryPanel";
+            this.InventoryPanel.Size = new System.Drawing.Size(825, 317);
+            this.InventoryPanel.TabIndex = 0;
+            this.InventoryPanel.Visible = false;
+            // 
+            // ClientPanel
+            // 
+            this.ClientPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ClientPanel.Controls.Add(this.label2);
+            this.ClientPanel.Location = new System.Drawing.Point(16, 12);
+            this.ClientPanel.Name = "ClientPanel";
+            this.ClientPanel.Size = new System.Drawing.Size(825, 317);
+            this.ClientPanel.TabIndex = 1;
+            this.ClientPanel.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Clients";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(709, 268);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 413);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.EmployeeButton);
+            this.Controls.Add(this.ClientsButton);
+            this.Controls.Add(this.InventoryButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "My HB Client";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.InventoryPanel.ResumeLayout(false);
+            this.InventoryPanel.PerformLayout();
+            this.ClientPanel.ResumeLayout(false);
+            this.ClientPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button InventoryButton;
+        private System.Windows.Forms.Button ClientsButton;
+        private System.Windows.Forms.Button EmployeeButton;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Panel InventoryPanel;
+        private System.Windows.Forms.Panel ClientPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ hb_desktop/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using System.Net.Http;
+
+namespace hb_desktop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MakePanelsNonvisible()
+        {
+            this.InventoryPanel.Visible = false;
+            this.ClientPanel.Visible = false;
+        }
+
+        private void InventoryButton_Click(object sender, EventArgs e)
+        {
+            MakePanelsNonvisible();
+            this.InventoryPanel.BringToFront();
+            this.InventoryPanel.Visible = true;
+
+            this.toolStripStatusLabel1.Text = "Retrieving Product Information...";
+            GetData("products");
+            this.toolStripStatusLabel1.Text = "Done.";
+        }
+
+        private void ClientsButton_Click(object sender, EventArgs e)
+        {
+            MakePanelsNonvisible();
+            this.ClientPanel.BringToFront();
+            this.ClientPanel.Visible = true;
+        }
+
+        private void GetData(String table)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            try
+            {
+                // PostgeSQL-style connection string
+                string connstring = String.Format("Server=Localhost;Port=5432;" +
+                    "User Id=postgres;Password=default;Database=testdb;");
+                // Making connection with Npgsql provider
+                NpgsqlConnection conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // quite complex sql statement
+                string sql = "SELECT * FROM " + table;
+                // data adapter making request from our connection
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, conn);
+                // i always reset DataSet before i do
+                // something with it.... i don't know why :-)
+                ds.Reset();
+                // filling DataSet with result from NpgsqlDataAdapter
+                da.Fill(ds);
+                // since it C# DataSet can handle multiple tables, we will select first
+                dt = ds.Tables[0];
+                // connect grid to DataTable
+                dataGridView1.DataSource = dt;
+                // since we only showing the result we don't need connection anymore
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                // something went wrong, and you wanna know why
+                MessageBox.Show(msg.ToString());
+                //throw;
+            }
+        }
+
+        private async Task<JsonObject> GetAsync(String table)
+        {
+            //TODO: Fix this (Ben)
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync()
+        }
+    }
+        
+}
